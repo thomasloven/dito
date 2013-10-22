0fc7fda54466f0a40fd7fa41316e7a35bf2fb1ba
Thomas Lovén <thomasloven@gmail.com>
2013-10-22 12:43:09 +0200

    Filesystem driver wrappers.

--- src/fs.c
+++ src/fs.c
@@ -1,19 +1,142 @@
 #include <fs.h>
+#include <stdlib.h>
+
+fs_driver_t ext2_driver; // temp
+
+fs_driver_t *supported[] = {
+  0, // unknown
+  &ext2_driver,
+  0, // fat16
+  0, // fat 32
+  0, // sfs
+  0, // ntfs
+  0, // hfs
+
+};
+
+fs_t *fs_load(partition_t *p, fs_type_t type)
+{
+  if(!p)
+    return 0;
+  if(type == unknown)
+    return 0;
+  if(!supported[type])
+    return 0;
+
+  fs_t *fs = malloc(sizeof(fs_t));
+  fs->p = p;
+  fs->type = type;
+  fs->data = 0;
+  fs->driver = supported[type];
+
+  if(fs->driver->hook_load)
+    fs->driver->hook_load(fs);
+
+  return fs;
+}
+
+fs_t *fs_create(partition_t *p, fs_type_t type)
+{
+  if(!p)
+    return 0;
+  if(type == unknown)
+    return 0;
+  if(!supported[type])
+    return 0;
+
+  fs_t *fs = malloc(sizeof(fs_t));
+  fs->p = p;
+  fs->type = type;
+  fs->data = 0;
+  fs->driver = supported[type];
+
+  if(fs->driver->hook_load)
+    fs->driver->hook_load(fs);
+
+  return fs;
+}
+
+void fs_close(fs_t *fs)
+{
+  if(!fs)
+    return;
+  if(fs->driver->hook_close)
+    fs->driver->hook_close(fs);
+
+  free(fs);
+}
+
+int fs_check(fs_t *fs)
+{
+  if(!fs)
+    return -1;
+  if(!fs->driver->hook_check)
+    return -1;
+  return fs->driver->hook_check(fs);
+}
+
+int fs_read(fs_t *fs, INODE ino, void *buffer, size_t length, size_t offset)
+{
+  if(!fs)
+    return 0;
+  if(!fs->driver->read)
+    return 0;
+  return fs->driver->read(fs, ino, buffer, length, offset);
+}
+
+int fs_write(fs_t *fs, INODE ino, void *buffer, size_t length, size_t offset)
+{
+  if(!fs)
+    return 0;
+  if(!fs->driver->write)
+    return 0;
+  return fs->driver->write(fs, ino, buffer, length, offset);
+}
+
+INODE fs_touch(fs_t *fs, fs_ftype_t type)
+{
+  if(!fs)
+    return 0;
+  if(!fs->driver->touch)
+    return 0;
+  return fs->driver->touch(fs, type);
+}
+
+dirent_t *fs_readdir(fs_t *fs, INODE dir, unsigned int num)
+{
+  if(!fs)
+    return 0;
+  if(!fs->driver->readdir)
+    return 0;
+  return fs->driver->readdir(fs, dir, num);
+}
+
+void fs_link(fs_t *fs, INODE ino, INODE dir, const char *name)
+{
+  if(!fs)
+    return;
+  if(fs->driver->link)
+    fs->driver->link(fs, ino, dir, name);
+}
+
+void fs_unlink(fs_t *fs, INODE dir, unsigned int num)
+{
+  if(!fs)
+    return;
+  if(fs->driver->unlink)
+    fs->driver->unlink(fs, dir, num);
+}
+
+fstat_t *fs_fstat(struct fs_st *fs, INODE ino)
+{
+  if(!fs)
+    return 0;
+  if(!fs->driver->fstat)
+    return 0;
+  return fs->driver->fstat(fs, ino);
+}
 
 
-fs_t *fs_load(partition_t *p, int type);
-fs_t *fs_create(partition_t *p, int type);
-void fs_close(fs_t *fs);
-int fs_check(fs_t *fs);
-
-int fs_read(fs_t *fs, INODE *ino, void *buffer, size_t length, size_t offset);
-int fs_write(fs_t *fs, INODE *ino, void *buffer, size_t length, size_t offset);
-INODE *fs_touch(fs_t *fs);
-dirent_t *fs_readdir(fs_t *fs, INODE *dir, unsigned int num);
-void fs_link(fs_t *fs, INODE *ino, INODE *dir, const char *name);
-void fs_unlink(fs_t *fs, INODE *dir, unsigned int num);
-int fs_fstat(struct fs_st *fs, INODE *ino);
-
-INODE *fs_finddir(fs_t *fs, INODE *dir, const char *name);
-INODE *fs_find(fs_t *fs, const char *path);
+INODE fs_finddir(fs_t *fs, INODE dir, const char *name);
+INODE fs_find(fs_t *fs, const char *path);
 
--- src/fs.h
+++ src/fs.h
@@ -23,52 +23,69 @@
 
 typedef unsigned int INODE;
 
+typedef enum fs_type_e {
+  unknown,
+  ext2,
+  fat16,
+  fat32,
+  sfs,
+  ntfs,
+  hfs
+}fs_type_t;
+
+typedef enum fs_ftype_e {
+  normal,
+  dir
+} fs_ftype_t;
+
 typedef struct
 {
   INODE ino;
   char *name;
 } dirent_t;
 
+typedef int fstat_t;
+
 struct fs_st;
 
 typedef struct
 {
-  int (*read)(struct fs_st *fs, INODE *ino, void *buffer, size_t length, size_t offset);
-  int (*write)(struct fs_st *fs, INODE *ino, void *buffer, size_t length, size_t offset);
-  INODE *(*touch)(struct fs_st *fs);
-  dirent_t *(*readdir)(struct fs_st *fs, INODE *dir, unsigned int num);
-  void (*link)(struct fs_st *fs, INODE *ino, INODE *dir, const char *name);
-  void (*unlink)(struct fs_st *fs, INODE *dir, unsigned int num);
-  int (*fstat)(struct fs_st *fs, INODE *ino);
-  INODE *root;
+  int (*read)(struct fs_st *fs, INODE ino, void *buffer, size_t length, size_t offset);
+  int (*write)(struct fs_st *fs, INODE ino, void *buffer, size_t length, size_t offset);
+  INODE (*touch)(struct fs_st *fs, fs_ftype_t type);
+  dirent_t *(*readdir)(struct fs_st *fs, INODE dir, unsigned int num);
+  void (*link)(struct fs_st *fs, INODE ino, INODE dir, const char *name);
+  void (*unlink)(struct fs_st *fs, INODE dir, unsigned int num);
+  fstat_t *(*fstat)(struct fs_st *fs, INODE ino);
+  INODE root;
 
   void *(*hook_load)(struct fs_st *fs);
   void *(*hook_create)(struct fs_st *fs);
   void (*hook_close)(struct fs_st *fs);
-  void (*hook_check)(struct fs_st *fs);
+  int (*hook_check)(struct fs_st *fs);
 } fs_driver_t;
 
 typedef struct fs_st
 {
   partition_t *p;
-  int type;
+  fs_type_t type;
   void *data;
-  fs_driver_t driver;
+  fs_driver_t *driver;
 } fs_t;
 
-fs_t *fs_load(partition_t *p, int type);
-fs_t *fs_create(partition_t *p, int type);
+fs_t *fs_load(partition_t *p, fs_type_t type);
+fs_t *fs_create(partition_t *p, fs_type_t type);
 void fs_close(fs_t *fs);
 int fs_check(fs_t *fs);
 
-int fs_read(fs_t *fs, INODE *ino, void *buffer, size_t length, size_t offset);
-int fs_write(fs_t *fs, INODE *ino, void *buffer, size_t length, size_t offset);
-INODE *fs_touch(fs_t *fs);
-dirent_t *fs_readdir(fs_t *fs, INODE *dir, unsigned int num);
-void fs_link(fs_t *fs, INODE *ino, INODE *dir, const char *name);
-void fs_unlink(fs_t *fs, INODE *dir, unsigned int num);
-int fs_fstat(struct fs_st *fs, INODE *ino);
+int fs_read(fs_t *fs, INODE ino, void *buffer, size_t length, size_t offset);
+int fs_write(fs_t *fs, INODE ino, void *buffer, size_t length, size_t offset);
+INODE fs_touch(fs_t *fs, fs_ftype_t type);
+dirent_t *fs_readdir(fs_t *fs, INODE dir, unsigned int num);
+void fs_link(fs_t *fs, INODE ino, INODE dir, const char *name);
+void fs_unlink(fs_t *fs, INODE dir, unsigned int num);
+fstat_t *fs_fstat(struct fs_st *fs, INODE ino);
 
-INODE *fs_finddir(fs_t *fs, INODE *dir, const char *name);
-INODE *fs_find(fs_t *fs, const char *path);
+INODE fs_finddir(fs_t *fs, INODE dir, const char *name);
+INODE fs_find(fs_t *fs, const char *path);
 
