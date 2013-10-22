d1c248b467a76b5477d38272e2f0a2a48802cc83
Thomas Lovén <thomasloven@gmail.com>
2013-10-22 12:08:16 +0200

    Filesystem framework outline.

--- src/fs.c
+++ src/fs.c
@@ -1,4 +1,19 @@
 #include <fs.h>
 
 
+fs_t *fs_load(partition_t *p, int type);
+fs_t *fs_create(partition_t *p, int type);
+void fs_close(fs_t *fs);
+int fs_check(fs_t *fs);
+
+int fs_read(fs_t *fs, INODE *ino, void *buffer, size_t length, size_t offset);
+int fs_write(fs_t *fs, INODE *ino, void *buffer, size_t length, size_t offset);
+INODE *fs_touch(fs_t *fs);
+dirent_t *fs_readdir(fs_t *fs, INODE *dir, unsigned int num);
+void fs_link(fs_t *fs, INODE *ino, INODE *dir, const char *name);
+void fs_unlink(fs_t *fs, INODE *dir, unsigned int num);
+int fs_fstat(struct fs_st *fs, INODE *ino);
+
+INODE *fs_finddir(fs_t *fs, INODE *dir, const char *name);
+INODE *fs_find(fs_t *fs, const char *path);
 
--- src/fs.h
+++ src/fs.h
@@ -6,21 +6,20 @@
 #define T_UNKNOWN 0
 #define T_ETX2 1
 
-// Needs support from driver
-// Read(ino)
-// Write(ino)
-// (ino) = Create()
-// (ino, name) = Readdir(dir_ino, num)
-// Link(ino, dir_ino, name)
-// Unlink(dir_ino, num)
+// Needs support from driver:
+// read(ino)
+// write(ino)
+// (ino) = touch()
+// (ino, name) = readdir(dir_ino, num)
+// link(ino, dir_ino, name)
+// unlink(dir_ino, num)
 // fstat(ino)
 //
 // Hooks in driver:
 // Load
+// Create
 // Close
-//
-//
-//
+// Check
 
 typedef unsigned int INODE;
 
@@ -34,14 +33,14 @@ struct fs_st;
 
 typedef struct
 {
-  int (*read)(struct fs_st *fs, INODE ino, void *buffer, size_t length, size_t offset);
-  int (*write)(struct fs_st *fs, INODE ino, void *buffer, size_t length, size_t offset);
-  INODE (*touch)(struct fs_st *fs);
-  dirent_t *(*readdir)(struct fs_st *fs, INODE dir, unsigned int num);
-  void (*link)(struct fs_st *fs, INODE ino, INODE dir, const char *name);
-  void (*unlink)(struct fs_st *fs, INODE dir, unsigned int num);
-  int (*fstat)(struct fs_st *fs, INODE ino);
-  INODE root;
+  int (*read)(struct fs_st *fs, INODE *ino, void *buffer, size_t length, size_t offset);
+  int (*write)(struct fs_st *fs, INODE *ino, void *buffer, size_t length, size_t offset);
+  INODE *(*touch)(struct fs_st *fs);
+  dirent_t *(*readdir)(struct fs_st *fs, INODE *dir, unsigned int num);
+  void (*link)(struct fs_st *fs, INODE *ino, INODE *dir, const char *name);
+  void (*unlink)(struct fs_st *fs, INODE *dir, unsigned int num);
+  int (*fstat)(struct fs_st *fs, INODE *ino);
+  INODE *root;
 
   void *(*hook_load)(struct fs_st *fs);
   void *(*hook_create)(struct fs_st *fs);
@@ -57,4 +56,19 @@ typedef struct fs_st
   fs_driver_t driver;
 } fs_t;
 
+fs_t *fs_load(partition_t *p, int type);
+fs_t *fs_create(partition_t *p, int type);
+void fs_close(fs_t *fs);
+int fs_check(fs_t *fs);
+
+int fs_read(fs_t *fs, INODE *ino, void *buffer, size_t length, size_t offset);
+int fs_write(fs_t *fs, INODE *ino, void *buffer, size_t length, size_t offset);
+INODE *fs_touch(fs_t *fs);
+dirent_t *fs_readdir(fs_t *fs, INODE *dir, unsigned int num);
+void fs_link(fs_t *fs, INODE *ino, INODE *dir, const char *name);
+void fs_unlink(fs_t *fs, INODE *dir, unsigned int num);
+int fs_fstat(struct fs_st *fs, INODE *ino);
+
+INODE *fs_finddir(fs_t *fs, INODE *dir, const char *name);
+INODE *fs_find(fs_t *fs, const char *path);
 
